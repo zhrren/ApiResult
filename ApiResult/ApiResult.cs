815a25800c3aab5e913c33a64faf5273b1861e5c
Mark <zhrren@qq.com>
2016-05-07 17:42:58 +0800

    重构

--- ApiResult/ApiResult.cs
+++ ApiResult/ApiResult.cs
@@ -2,21 +2,6 @@
 
 namespace Mark.ApiResult
 {
-
-    public enum ApiCode
-    {
-        Success = 0,
-        Unauthorized = 1,
-        NotFound = 2,
-        InternalServerError = 3,
-        UnsupportedMediaType = 4,
-        BadRequest = 5,
-        NotSupport = 6,
-        ModelValidationError = 7,
-        RequestWarn = 8,
-        RequestReject = 9
-    }
-
     public class ApiResult : ApiResult<object>
     {
         public ApiResult()
@@ -37,29 +22,10 @@ namespace Mark.ApiResult
         /// <summary>
         /// 操作失败
         /// </summary>
-        /// <param name="code">ref ApiCode</param>
-        /// <param name="message"></param>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public ApiResult(ApiCode code, object data = null) : base(code, data) { }
-
-        /// <summary>
-        /// 操作失败
-        /// </summary>
-        /// <param name="typeCode">大于0且小于1000，全局唯一</param>
-        /// <param name="errorCode">大于0且小于1000</param>
-        /// <param name="message"></param>
-        public ApiResult(int typeCode, int errorCode, string message, object data = null)
-            : base(typeCode, errorCode, message, data)
-        { }
-
-        /// <summary>
-        /// 操作失败
-        /// </summary>
         /// <param name="typeCode">大于0且小于1000，全局唯一</param>
         /// <param name="message"></param>
-        public ApiResult(int typeCode, string message, object data = null)
-            : base(typeCode, message, data)
+        public ApiResult(int error, string message, object data = null)
+            : base(error, message, data)
         { }
     }
 
@@ -89,40 +55,6 @@ namespace Mark.ApiResult
         /// </summary>
         public ApiResult(TData data = null, string message = "OK")
         {
-            Code = (int)ApiCode.Success;
-            Message = message;
-            Data = data;
-        }
-
-        /// <summary>
-        /// 操作失败
-        /// </summary>
-        /// <param name="code">ref ApiCode</param>
-        /// <param name="message"></param>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public ApiResult(ApiCode code, TData data = null)
-        {
-            Code = (int)code;
-            Message = code.ToString();
-            Data = data;
-        }
-
-        /// <summary>
-        /// 操作失败
-        /// </summary>
-        /// <param name="typeCode">大于0且小于1000，全局唯一</param>
-        /// <param name="errorCode">大于0且小于1000</param>
-        /// <param name="message"></param>
-        public ApiResult(int typeCode, int errorCode, string message, TData data = null)
-        {
-            if (typeCode <= 0 || typeCode >= 10000)
-                throw new ArgumentOutOfRangeException("type", "必须大于0且小于1000");
-
-            if (errorCode <= 0 || errorCode >= 10000)
-                throw new ArgumentOutOfRangeException("code", "必须大于0且小于1000");
-
-            Code = typeCode * 10000 + errorCode;
             Message = message;
             Data = data;
         }
@@ -132,12 +64,9 @@ namespace Mark.ApiResult
         /// </summary>
         /// <param name="typeCode">大于0且小于1000，全局唯一</param>
         /// <param name="message"></param>
-        public ApiResult(int typeCode, string message, TData data = null)
+        public ApiResult(int error, string message, TData data = null)
         {
-            if (typeCode <= 0 || typeCode >= 10000)
-                throw new ArgumentOutOfRangeException("type", "必须大于0且小于1000");
-
-            Code = typeCode * 10000;
+            Code = error;
             Message = message;
             Data = data;
         }
